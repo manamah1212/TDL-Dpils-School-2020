2ee2c459108e7d01e02765fdffa9692c59b0f294
SIA TestDevLab <siatestdevlab@school.local>
2020-09-08 11:30:15 +0300

    sort js

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+/*
 // Loops
 
 // for loop
@@ -66,5 +67,90 @@ for (let i = 0; i < ages.length; i++){
         console.log('You too young for alco');
     }
 }
-
+*/
 //comment
+/*
+function greet(){
+    console.log('Hi!');
+}
+
+//greet();
+//greet();
+
+
+
+//function expression
+const speak = function(name = 'Maris', surname = 'Petro'){//name,surname - tut vvod defolt znacenie
+    console.log(`Hi ${name} ${surname}`);
+}
+speak('Jevgenij','Greibo');
+
+const calculateArea = function(radius){
+    let area = 3.14 * radius**2;
+    return area;
+}
+const myCircle = calculateArea(5);
+console.log(myCircle);
+
+//arrow funcions
+
+const arrowCalculateArea = (radius) => 3.14 * radius**2;
+
+
+console.log(`Area is: ${arrowCalculateArea(5)}`);
+
+const bill = function(product, tax) {
+    let total = 0
+    for(let i =0 ; i < product.lenght;i++){
+        total += product[i]+product[i] * tax;
+    }
+    return total;
+}
+//console.log(bill(50,9 ));
+
+//let tax = 5;
+
+const billk = (product,tax ) => {
+    let total = 0;
+    for(let i =0 ; i < product.lenght;i++){
+        total += product[i]+product[i] * tax;
+    }
+    return total;
+}
+console.log(`Area is: ${billk(1,5)}`);
+//forEach
+
+let people = ['Max','Deniss','Alex','Ivan'];
+
+//for(let i =0 ;i<people.length;i++){
+//    console.log(people[i]);             //viviod tekst massiva 
+//}
+
+people.forEach(person => {
+    console.log(person);        //vivod tekstovogo massiva
+});
+*/
+//console.log(sortArray(array));
+//array.forEach(person => {
+    
+    //console.log(person);        //vivod tekstovogo massiva
+//});
+
+let array = [89,46,0,35,1,-2,35];
+
+let sortArray = (array) => {
+    let len = array.length; // do kakogo elementa dvigatj - pointer
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j]; // dvigajet vpravo esli bolse
+                array[j] = array[j + 1];//peredvigajet mensee nazat
+                array[j + 1] = tmp; // soxranaet peremennuju iz vremenoj
+                //console.log(array)//vivod iteracii
+            }
+        }
+    }
+    return array;
+};
+console.log(`sorted array is: ${sortArray(array)}`);
+console.log(array);
