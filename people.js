3c2bc8115af92a006a68db081b1d4e0e60a8d5a9
SIA TestDevLab <siatestdevlab@school.local>
2020-09-09 11:43:41 +0300

    bufer i td

---
+++ people.js
@@ -0,0 +1,10 @@
+const people = ['Deniss','Alex','Max'];
+const ages = [20,25,30];
+//console.log(people);
+
+module.exports = {
+    people: people,
+    ages: ages
+}; //export v drugie faili
+
+//shoulde export people lenght of array and ages langht
