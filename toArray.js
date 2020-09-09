74b88f98e58506f11252bc658f0bf6a2886f9700
SIA TestDevLab <siatestdevlab@school.local>
2020-09-09 09:23:21 +0300

    toArray

--- toArray.js
+++ toArray.js
@@ -1,9 +1,10 @@
-const latters = {a:1 , b:1};
+const latters = {a: 1 ,
+    b: 2};
 let arr = 100;
 let letters2 = function (transform){
 
-    console.log(Object.values(latters));
- 
+console.log(Object.values(latters));
+
 }
 
 const test = Object.keys(latters);
@@ -12,4 +13,31 @@ const test2 = Object.values(latters);
 
 const entries = Object.entries(latters);
 const newArray = entries[0].concat(entries[1]);
-console.log(newArray)
+//console.log(newArray)
+
+
+
+const toArrayFunkc = function(){
+var a=[], b = latters;
+a.push(latters.a , latters.b);
+var newData = [a,b];
+console.log(newData)
+}
+toArrayFunkc();
+
+function saveToArray() {
+// const save = [];
+const bigArray = [];
+let save = Array.from(latters); // (*)
+for (let lat in latters) {
+if (latters.hasOwnProperty(lat)) {
+//save.push(lat ,latters.a,latters.b);
+save.push(lat + ":" + latters[lat]);
+}
+
+}
+
+console.log(save);
+
+}
+saveToArray();
