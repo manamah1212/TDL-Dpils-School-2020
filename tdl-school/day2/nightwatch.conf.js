8013e0994b56a9290c20cb0da36f67feaeb8626b
SIA TestDevLab <siatestdevlab@school.local>
2020-09-11 09:27:06 +0300

    day2

---
+++ tdl-school/day2/nightwatch.conf.js
@@ -0,0 +1,65 @@
+module.exports = {
+	src_folders: ["tests"],
+
+	webdriver: {
+		start_process: true,
+		port: 4444,
+	},
+
+	test_settings: {
+		default: {
+			desiredCapabilities: {
+				browserName: "chrome",
+			},
+			webdriver: {
+				server_path: "node_modules/.bin/chromedriver"
+			},
+		},
+
+		firefox: {
+			browserName: "firefox",
+			webdriver: {
+				server_path: "node_modules/.bin/geckodriver"
+			},
+		},
+
+		chrome_extra: {
+			desiredCapabilities: {
+				browserName: "chrome",
+				chromeOptions: {
+					args: [
+						"no-sandbox",
+						"use-fake-device-for-media-stream",
+						// "use-file-for-fake-video-capture=PATH_TO_FILE",
+						// "use-file-for-fake-audio-capture=PATH_TO_FILE",
+					],
+					prefs: {
+						profile: {
+							content_settings: {
+								exceptions: {
+									media_stream_camera: {
+										"https://*,*": {
+											setting: 1,
+										},
+									},
+									media_stream_mic: {
+										"https://*,*": {
+											setting: 1,
+										},
+									},
+								},
+							},
+							default_content_setting_values: {
+								automatic_downloads: 1,
+								notifications: 1,
+							},
+						},
+					},
+				},
+			},
+			webdriver: {
+				server_path: "node_modules/.bin/chromedriver"
+			},
+		},
+	},
+};
